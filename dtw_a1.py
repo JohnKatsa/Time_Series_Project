816ed54cbd6fcdf4ceaa50673c9d1654b3d68cd3
JohnKatsa <johnkatsa66@gmail.com>
2018-05-22 21:40:11 +0300

    need to 1)do dtw for all, 2)read train set properly

--- dtw_a1.py
+++ dtw_a1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from ast import literal_eval
 import csv
 from dtw import dtw
@@ -29,18 +30,22 @@ for x in trajectory_list[4]:
 
 # read train set and make lon-lat lists
 trajectory_list = []
-with open('./train_set_a1.csv','rb') as csvfile:
+with open('./train_set.csv','rb') as csvfile:
     for row in csvfile:
         trajectory_list.append(row)
 trajectory_list.pop(0)
 trajectory_list = [literal_eval(x) for x in trajectory_list]
 
-# get for each pair the closest
+# get for each pair the dtw distances and store them in closest
 for trip in trajectory_list:
     lonlattest = []
     closest = []
+    index = 0
     for x in trip:
         lonlattest.append((x[1],x[2]))
-    dtw(lonlat1,lonlattest,dist=harvesine)
+    d = dtw(lonlat1,lonlattest,dist=harvesine)
+    closest.append((index,d))
+    index+=1
 
-print lon
+closest = np.asarray(closest)
+minfive = arr.argsort()[:5]
