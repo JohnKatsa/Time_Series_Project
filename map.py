a0ebd3def0e37f0a8a72eb5481b5809685afd411
JohnKatsa <johnkatsa66@gmail.com>
2018-05-25 16:54:32 +0300

    map ready

--- map.py
+++ map.py
@@ -6,22 +6,31 @@ import gmplot
 trainSet = pd.read_csv(
 './train_set.csv', # replace with the correct path
 converters={"Trajectory": literal_eval},
-index_col='tripId'
+index_col='tripId',
+nrows=1000
 )
 
-filenames = ["my_map1.html","my_map2.html","my_map4.html","my_map5.html","my_map6.html"]
+filenames = ["mymap1.html","mymap2.html","mymap3.html","mymap4.html","mymap5.html"]
+
 fi = 0;
 
 latlong = []
 
-for i in trainSet['Trajectory']:        # for every journeypattern
+checklist = []
+
+for i,jpid in zip(trainSet['Trajectory'],trainSet["journeyPatternId"]):        # for every journeypattern
+    if jpid in checklist:
+        continue;
+    checklist.append(jpid)
     for j in i:
         latlong.append((j[2],j[1]))     # make list of latitudes and longtitudes
         #print latlong
-        gmap = gmplot.GoogleMapPlotter(latlong[0][0], latlong[0][1], 13)
-        lats, lons = zip(*latlong)
-        gmap.plot(lats, lons, 'cornflowerblue', edge_width=5)
-        # Draw
-        gmap.draw(filenames[fi])
+    gmap = gmplot.GoogleMapPlotter(latlong[0][0], latlong[0][1], 13)
+    lats, lons = zip(*latlong)
+    gmap.plot(lats, lons, 'cornflowerblue', edge_width=5)
+    # Draw
+    gmap.draw(filenames[fi])
     fi += 1;
     latlong = []
+    if fi == 5:
+        break
