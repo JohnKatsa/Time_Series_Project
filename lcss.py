b6dd8df1771d9e7c96bb8eab842e7370685ee9db
Nikos <nikotheract11@gmail.com>
2018-05-26 18:03:35 +0300

    lccs ready

--- lcss.py
+++ lcss.py
@@ -8,25 +8,25 @@ import gmplot
 
 def cmp(x,y):
 	if x[0] > y[0]:
-            return -1
-        elif x[0] == y[0]:
-            return 0
+			return -1
+	elif x[0] == y[0]:
+		return 0
 	return 1
 def haversine(x,y):
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [x[1], x[2], y[1], y[2]])
+	"""
+	Calculate the great circle distance between two points
+	on the earth (specified in decimal degrees)
+	"""
+	# convert decimal degrees to radians
+	lon1, lat1, lon2, lat2 = map(radians, [x[1], x[2], y[1], y[2]])
 
-    # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+	# haversine formula
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+	c = 2 * asin(sqrt(a))
+	r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+	return c * r
 
 
 # X and Y lists with m , n elements respectively
@@ -65,11 +65,11 @@ def backtrack(C,X,Y,i,j):
 
 
 trainSet = pd.read_csv('./train_set.csv',converters={"Trajectory": literal_eval},index_col='tripId',nrows=1500)
-trainSet = trainSet[0:500]
+trainSet = trainSet[0:1200]
 trajectory_list = []
 with open('./test_set_a2.csv','rb') as csvfile:
-    for row in csvfile:
-        trajectory_list.append(row)
+	for row in csvfile:
+		trajectory_list.append(row)
 trajectory_list.pop(0)
 trajectory_list = [literal_eval(x) for x in trajectory_list]
 
@@ -79,19 +79,21 @@ start_time = time.time()
 
 filenames = ["my_map1.html","my_map2.html","my_map4.html","my_map5.html","my_map6.html"]
 fi=0
-for route in trajectory_list:	
+counter = 0
+for route in trajectory_list:
+	file = "original" + str(counter)+".html"
 	latlong = []
 	for j in route:
 		latlong.append((j[2],j[1]))
 	gmap = gmplot.GoogleMapPlotter(latlong[0][0], latlong[0][1], 13)
 	lats, lons = zip(*latlong)
 	gmap.plot(lats, lons, 'cornflowerblue', edge_width=5)
-	gmap.draw("original.html")
+	gmap.draw(file)
 
 	cm=[]
 	for line,jpid in zip(trainSet["Trajectory"],trainSet["journeyPatternId"]):
 		C = lcss(route,line)
-	#	print C[len(route)-1][len(line)-1]
+		print C[len(route)-1][len(line)-1]
 		cm.append([C[len(route)-1][len(line)-1],backtrack(C,route,line,len(route)-1,len(line)-1),jpid,route])
 	cm.sort(cmp=cmp)
 	latlong = []
@@ -101,23 +103,26 @@ for route in trajectory_list:
 	   #print cm[i][2:]
 		latlong = []
 		latsl = []
+		file = "map" + str(counter)+ "_"+str(fi)+".html"
 		for j in cm[i][3]:
-	   		latlong.append((j[2],j[1]))
-	   	for jj in cm[i][1]:
-	   		latsl.append((jj[2],jj[1]))
+			latlong.append((j[2],j[1]))
+		for jj in cm[i][1]:
+			latsl.append((jj[2],jj[1]))
 
-	   	gmap = gmplot.GoogleMapPlotter(latlong[i][0], latlong[i][1], 13)
-	   	lats, lons = zip(*latlong)
+		gmap = gmplot.GoogleMapPlotter(latlong[i][0], latlong[i][1], 13)
+		lats, lons = zip(*latlong)
 		gmap.plot(lats, lons, 'cornflowerblue', edge_width=5)
-	   	
-	   	if len(cm[i][1]) > 0:
-	   		#gmap = gmplot.GoogleMapPlotter(latsl[0][0], latsl[0][1], 19)
-	   		lats2, lons2 = zip(*latsl)
-	   		gmap.plot(lats2,lons2,'red',edge_width=6)
-    	gmap.draw(filenames[fi])
-    	fi += 1;
-    	if fi >= 5:
-    		break
-total_time = time.time()-start_time#  time.time()
+		
+		if len(cm[i][1]) > 0:
+			#gmap = gmplot.GoogleMapPlotter(latsl[0][0], latsl[0][1], 19)
+			lats2, lons2 = zip(*latsl)
+			gmap.plot(lats2,lons2,'red',edge_width=6)
+		gmap.draw(file)
+		fi += 1
+		#print fi,i
+		if fi >= 5:
+			break
+	counter+=1
+	total_time = time.time()-start_time#  time.time()
 print "total_time:",total_time
 
