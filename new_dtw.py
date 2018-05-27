2ff1db48ff77fd3da9da641071f6d020d587be63
JohnKatsa <johnkatsa66@gmail.com>
2018-05-27 16:35:26 +0300

    sample dataset

--- new_dtw.py
+++ new_dtw.py
@@ -89,12 +89,18 @@ def cross_validation(data,k):
         success_rate += float(success/len(realjpid))
         #print success_rate
 
+        print k, " fold ended"
+
     success_rate = success_rate/k
     print "total success rate = ", success_rate
 
 # initialize time
 start_time = time.time()
-trainSet = pd.read_csv('./train_set.csv',converters={"Trajectory": literal_eval},index_col='tripId',nrows=700)
+
+n = sum(1 for line in open('./train_set.csv')) - 1 #number of records in file (excludes header)
+s = 50 #desired sample size
+skip = sorted(random.sample(xrange(1,n+1),n-s)) #the 0-indexed header will not be included in the skip list
+trainSet = pd.read_csv('./train_set.csv',converters={"Trajectory": literal_eval},index_col='tripId', skiprows=skip)
 
 
 # read test set and make lon-lat lists
